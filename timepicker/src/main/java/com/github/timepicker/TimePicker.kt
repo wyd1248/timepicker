fd73fec944d2b7db222c2f05e2fffd6a7d2a7984
Yanda Wu <wyd248@gmail.com>
2021-04-07 17:02:32 -0700

    upload to maven

--- timepicker/src/main/java/com/github/timepicker/TimePicker.kt
+++ timepicker/src/main/java/com/github/timepicker/TimePicker.kt
@@ -1,11 +1,18 @@
 package com.github.timepicker
 
 import android.content.Context
+import android.widget.TimePicker
 import android.widget.Toast
 
 object TimePicker {
 
     fun show(context: Context, string: String) {
         Toast.makeText(context, string, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showTimePicker(context: Context) {
+        TimePicker(context).setOnTimeChangedListener { _, hourOfDay, minute ->
+            show(context, "$hourOfDay and $minute")
+        }
     }
 }
